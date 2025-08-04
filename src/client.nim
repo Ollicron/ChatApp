4e04efacec1d5e201c49fb5de42ca78692da5641
Ollicron <Luky.romero03@gmail.com>
2025-08-04 16:25:10 -0400

    Added all the files

---
+++ src/client.nim
@@ -0,0 +1,14 @@
+import 
+    os,threadpool
+
+echo ("Initiating Chat Client")
+if paramCount() == 0:
+    quit("Please specify the server address, e.g. ./client localhost")
+
+let serverAddr = paramStr(1)
+echo ("Connecting to ",serverAddr)
+
+while true:
+    let message = spawn stdin.readLine()
+    echo("Sending \"", ^message,"\"")
+
