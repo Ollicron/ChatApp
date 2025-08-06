f951815e563747587bb3bd1e31674290231266f8
Ollicron <Luky.romero03@gmail.com>
2025-08-06 19:36:57 -0400

    Wrote some async functions for the server!

--- src/server.nim
+++ src/server.nim
@@ -1,16 +1,48 @@
 import asyncdispatch, asyncnet
 
-
 type
-  Client = ref object
-    socket: AsyncSocket
-    netAddr: string
-    id: int
-    connected: bool
+    Client = ref object
+        socket: AsyncSocket
+        netAddr: string
+        id: int
+        connected: bool
 
-  Server = ref object
-    socket: AsyncSocket
-    clients: seq[Client]
+    Server = ref object
+        socket: AsyncSocket
+        clients: seq[Client]
 
-proc newServer*(): Server=
-  Server(socket:newAsyncSocket, clients: @[])
+# This function creates a new server and returns it.
+proc makeNewServer*(port:int = 7777): Server = 
+    let newServer = Server(socket: newAsyncSocket(), clients: @[])
+    # Bind the address and port number, default address is localhost.
+    newServer.socket.bindAddr(Port(port))
+    # Ready the server socket for new connections.
+    newServer.socket.listen()
+    echo("Listening on localhost:", port)
+    return newServer
+
+# Because we want this to be ongoing we need this to run forever.
+proc acceptConnections(server: Server){.async.} =
+    while true:
+        # Pause execution of this procedure until a new connection is accepted.
+        let (clientAddr,clientSocket) = await server.socket.acceptAddr()
+
+        echo ("Accepted connection from:",clientAddr)
+        # Populate a new client object and store it.
+        let newClient = Client(
+            socket: clientSocket,
+            netAddr: clientAddr,
+            id:0,
+            connected: true
+        )
+        server.clients.add(newClient)
+
+
+when isMainModule:
+    let someServer = makeNewServer()
+    
+    #[ We are discarding the future, but we want this to run asynchronously with run forever ]#
+    discard acceptConnections(someServer)
+
+
+    runForever()
